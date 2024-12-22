a517ef401724134919ab3fb1a79082cd6c054b81
yangwenbing <yangwenbing2@163.com>
2024-12-22 21:15:11 +0800

    增加用户相关的API

--- xgkwx_common/src/main/java/com/yuxi/xgkwx/common/exception/CommonExceptionEnums.java
+++ xgkwx_common/src/main/java/com/yuxi/xgkwx/common/exception/CommonExceptionEnums.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum CommonExceptionEnums {
 
     SERVER_ERROR("E00000", "系统异常"),
-    MESSAGE_TYPE_ERROR("E00001", "消息类型错误")
+    MESSAGE_TYPE_ERROR("E00001", "消息类型错误"),
+    USER_NOT_FOUND("E00002", "用户不存在"),
+    PASSWORD_ERROR("E00003", "密码错误")
     ;
 
     private String code;
